12bb056edceca7bfc03e837f35e483af2140dccc
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2024-03-18 19:55:28 -0300

    Aprimoramento de Código

--- desafio-01/clinica.py
+++ desafio-01/clinica.py
@@ -4,14 +4,14 @@ def title(text):
     print('~' * 40)
 
 class Usuario:
-    def __init__(self, nome_funcionario):
-        self.name = nome_funcionario
+    def __init__(self, nome_usuario):
+        self.name = nome_usuario
     def register(self):
         return self.__dict__
     
 class Paciente(Usuario):
-    def __init__(self, nome_funcionario):
-        super().__init__(nome_funcionario)
+    def __init__(self, nome_paciente):
+        super().__init__(nome_paciente)
 
 class Funcionario(Usuario):
     def __init__(self, nome_funcionario, especialidade):
@@ -35,6 +35,20 @@ class Recepcionista(Funcionario):
         print()
         return consulta
     
+    def agendarExame(self, nome_paciente, data_consulta, horas, nome_medico, especialidade, procedimento):
+        exame = {
+            'paciente': nome_paciente,
+            'data': data_consulta,
+            'horario': horas,
+            'medico': nome_medico,
+            'especialidade': especialidade,
+            'procedimento': procedimento
+        }
+        print()
+        print(f"{exame['procedimento'].title()} marcada para o dia {exame['data']} às {exame['horario']} com especialista {exame['medico']} da especialidade {exame['especialidade']}")
+        print()
+        return exame
+    
 class Medico(Funcionario):
     def __init__(self, nome_funcionario, especialidade, crm):
         super().__init__(nome_funcionario, especialidade)
@@ -49,6 +63,11 @@ class Medico(Funcionario):
         }
 
         return receita
+    
+    def realizarExame(self, exame):
+        print()
+        print(f"{exame['procedimento'].title()} realizado no dia {exame['data']} às {exame['horario']} pelo especialista {exame['medico']} da especialidade {exame['especialidade']}")
+        print()
         
        
 class Enfermeiro(Funcionario):
@@ -73,6 +92,7 @@ pac = Paciente('Gabriel')
 enf = Enfermeiro('Isabel', 'enfermeira', '0123456789')
 
 cons = recep.agendarConsulta(pac.name, '07/03/2024', '8h40', med.name, med.specialty)
+ex = recep.agendarExame(pac.name, '07/03/2024', '6h30', med.name, med.specialty, 'Hemograma')
 receita = med.prescricao(pac.name, 'Racutan')
 aplicacao = enf.medicacao(pac.name, 'Bezetacil')
 
@@ -80,6 +100,13 @@ title('Consulta')
 for d in cons.items():
     print(f'{d[0]}: {d[1]}')
 print()
+
+title('Exame')
+for d in ex.items():
+    print(f'{d[0]}: {d[1]}')
+print()
+
+re = med.realizarExame(ex)
 
 title('Receita')
 for d in receita.items():
